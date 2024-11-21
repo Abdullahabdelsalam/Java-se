b8f33cda3ad1a6c85feb668e3f24eec90c0311f7
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-11-21 23:52:45 +0200

    Create TestAverge.java

---
+++ JavaSEBasics/src/javase/essentials/basics/TestAverge.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javase.essentials.basics;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class TestAverge {
+    public static void main(String[] args) {
+        int firstNumber = 6;
+        int secondNumber = 4;
+        int thirdNumber = 9;
+        
+       double averageResult = (firstNumber + secondNumber + thirdNumber) / 3.0;
+        System.out.println("average = " + averageResult);
+    }
+}
