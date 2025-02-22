bc98f99ddec1d0b91c53669c83342d890cb115de
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-02-22 23:20:38 +0200

    Create InvalidAmountException.java

---
+++ Exception Handling/Banking/src/banking/InvalidAmountException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banking;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class InvalidAmountException extends Exception{
+
+    public InvalidAmountException() {
+    }
+
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+    
+    
+}
