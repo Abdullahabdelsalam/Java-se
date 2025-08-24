00afdd4a94e1245e177c540990215d97cc826720
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-08-25 01:15:05 +0300

    Create Waiter.java

---
+++ EraaSoft/javaSEa/src/javaOOP/oop2/Waiter.java
@@ -0,0 +1,17 @@
+package javaOOP.oop2;
+
+public class Waiter extends Employee implements Eatable {
+    public Waiter(String name, int age, double salary) {
+        super(name, age, salary,  "Waiter");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Waiter " + getName() + " is having a quick meal break.");
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Waiter " + getName() + " is serving customers.");
+    }
+}
