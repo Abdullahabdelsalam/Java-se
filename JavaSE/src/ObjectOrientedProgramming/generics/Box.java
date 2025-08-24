4099983bf423efe52248b37f36c6ab7822715ef9
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-08-25 01:16:00 +0300

    Create Box.java

---
+++ JavaSE/src/ObjectOrientedProgramming/generics/Box.java
@@ -0,0 +1,14 @@
+package ObjectOrientedProgramming.generics;
+
+public class Box<T> {
+    T item;
+
+    public void setItem(T item){
+        this.item = item;
+    }
+
+    public T getItem(){
+        return item;
+    }
+
+}
