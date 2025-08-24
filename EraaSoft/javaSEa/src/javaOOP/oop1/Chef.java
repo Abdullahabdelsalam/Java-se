1ed3a83aba2ca4986eeb319aa1a783f4641d3a7e
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-08-25 01:14:15 +0300

    Create Chef.java

---
+++ EraaSoft/javaSEa/src/javaOOP/oop1/Chef.java
@@ -0,0 +1,11 @@
+package javaOOP.oop1;
+
+public class Chef extends Employee{
+    public Chef(String name, int age, double salary, String role) {
+        super(name, age, salary, role);
+    }
+    @Override
+    public void work() {
+        System.out.println("Chef " + getName() + " is preparing dishes");
+    }
+}
