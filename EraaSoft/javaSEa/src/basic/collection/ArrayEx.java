6df0a1d87fdaaa8274baa5061910e7977cccae02
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-08-26 22:25:28 +0300

    Create ArrayEx.java

---
+++ EraaSoft/javaSEa/src/basic/collection/ArrayEx.java
@@ -0,0 +1,10 @@
+package basic.collection;
+
+public class ArrayEx {
+    public static void main(String[] args) {
+        String[] emp = {"Abdullah", "Muhammed", "Ali", "Omer"};
+        for(String item : emp){
+            System.out.println(item);
+        }
+    }
+}
